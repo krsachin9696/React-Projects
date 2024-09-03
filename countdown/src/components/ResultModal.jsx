ba52935adb943aaaee19322adc71546edbe8a902
krsachin9696 <krsachin9696@gmail.com>
2024-09-03 16:14:37 +0530

    useImaperativeHandle

--- countdown/src/components/ResultModal.jsx
+++ countdown/src/components/ResultModal.jsx
@@ -1,8 +1,19 @@
-import { forwardRef } from "react"
+import { forwardRef, useImperativeHandle, useRef } from "react"
 
 const ResultModal = forwardRef (function ResultModal({result, targetTime }, ref) {
+
+    const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        };
+    }); 
+
     return (
-        <dialog ref={ref} className="result-modal">
+        <div ref={dialog} className="result-modal">
             <h2>You {result}</h2>
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
@@ -13,7 +24,7 @@ const ResultModal = forwardRef (function ResultModal({result, targetTime }, ref)
             <form method="dialog">
                 <button>Close</button>
             </form>
-        </dialog>
+        </div>
     )
 })
 
