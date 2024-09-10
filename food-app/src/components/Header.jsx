3e548acb9c735203d65b4569748cd36b69a4df2e
krsachin9696 <krsachin9696@gmail.com>
2024-09-10 17:11:24 +0530

    home page

---
+++ food-app/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import logoImg from "../assets/logo.jpg";
+import Button from "../UI/Button";
+import CartContext from "../store/cartContext";
+
+export default function Header() {
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
+    return (
+        <header id="main-header">
+            <div id="title">
+                <img src={logoImg} alt="Logo" />
+                <h1>React-Food</h1>
+            </div>
+            <nav>
+                <Button textOnly>Cart ({totalCartItems})</Button>
+            </nav>
+        </header>
+    )
+}
