bb7e86e826da3bd507e872f3108906452c078b20
krsachin9696 <krsachin9696@gmail.com>
2024-09-10 11:50:45 +0530

    food app

--- food-app/backend/app.js
+++ food-app/backend/app.js
@@ -66,4 +66,6 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log(`Backend is running on PORT: 3000`);
+});
